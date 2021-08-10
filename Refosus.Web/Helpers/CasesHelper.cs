9c124924e4a2c35eb5dec7805d6277300f2b893d
nativa <nativa@refocosta.com>
2021-08-10 08:17:54 -0500

    Cron para casos vencidos
    Cron para casos vencidos

---
+++ Refosus.Web/Helpers/CasesHelper.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Refosus.Web.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Refosus.Web.Helpers
+{
+    public class CasesHelper : IHostedService, IDisposable
+    {
+        private ILogger<CasesHelper> logger;
+        private Timer timer;
+
+        public IServiceScopeFactory serviceScopeFactory;
+
+        public CasesHelper(ILogger<CasesHelper> logger, IServiceScopeFactory _serviceScopeFactory)
+        {
+            this.logger = logger;
+            this.serviceScopeFactory = _serviceScopeFactory;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var now = System.DateTime.Now.ToUniversalTime();
+                var ctx = scope.ServiceProvider.GetRequiredService<DataContext>();
+                var cases = ctx.CaseEntity.Where(x => x.Status == 1 && now > x.DeadLine).ToList();
+                if (cases != null)
+                {
+                    foreach (var item in cases)
+                    {
+                        item.Status = 3;
+                        ctx.SaveChanges();
+                        logger.LogInformation("Cambios efectuados");
+                    }
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
